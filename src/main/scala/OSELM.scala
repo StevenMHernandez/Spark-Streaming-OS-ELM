2573dd5d9a7805e0e9b4cc55e67492735ad9eb39
StevenMHernandez <contact@stevenhdesigns.com>
2018-11-04 21:25:17 -0500

    complete ELM portion of OS-ELM model, handle normalization/denormalization, fix bug with loaded CSV matrix data

--- src/main/scala/OSELM.scala
+++ src/main/scala/OSELM.scala
@@ -1,8 +1,10 @@
 import org.apache.spark._
 import breeze.linalg._
+import breeze.numerics._
+import breeze.stats.distributions.Rand
 
 object OSELM {
-  val L_HIDDEN_NODES = 10
+  val L_HIDDEN_NODES = 50
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
@@ -10,15 +12,17 @@ object OSELM {
     conf.setAppName("Indoor Localization with OS-ELM")
     val sc = new SparkContext(conf)
 
-    val trX = loadCSV(sc, "src/main/resources/training_input.csv")
-    val trY = loadCSV(sc, "src/main/resources/training_output.csv")
-    val teX = loadCSV(sc, "src/main/resources/testing_input.csv")
-    val teY = loadCSV(sc, "src/main/resources/testing_output.csv")
+    val (trX, maxX, minX) = normalizeMatrix(loadCSV(sc, "src/main/resources/training_input.csv"))
+    val (trY, maxY, minY) = normalizeMatrix(loadCSV(sc, "src/main/resources/training_output.csv"))
+    val (teX, _, _) = normalizeMatrix(loadCSV(sc, "src/main/resources/testing_input.csv"), maxX, minX)
+    val (teY, _, _) = normalizeMatrix(loadCSV(sc, "src/main/resources/testing_output.csv"), maxY, minY)
 
     val l = trX.rows
-    val dim = trX.cols
-    val rangeInput = 0 until l
-    val rangeHiddenLayers = 0 until L_HIDDEN_NODES
+    val n = trX.cols
+    val m = trY.cols
+
+    val rangeInputs = 0 until l
+    val rangeTestInputs = 0 until teX.rows
 
     val input = trX
     val T = trY
@@ -28,30 +32,29 @@ object OSELM {
       * Basic ELM Learning
       *
       */
+    val a = DenseMatrix.rand(L_HIDDEN_NODES, n, Rand.gaussian)
+    val bias = DenseVector.rand(L_HIDDEN_NODES, Rand.gaussian)
 
-    def hardLim(a: Double, b: Double, x: Double) = {
-      if (a * x + b >= 0) 1 else 0
-    }
+    val H = buildHiddenLayer(a, bias, input)
 
-    var a = DenseVector.rand(L_HIDDEN_NODES) * DenseVector.fill(L_HIDDEN_NODES){2.0} - DenseVector.ones[Double](L_HIDDEN_NODES)
-    var bias = DenseVector.rand(L_HIDDEN_NODES) * DenseVector.fill(L_HIDDEN_NODES){2.0} - DenseVector.ones[Double](L_HIDDEN_NODES)
+    val K_0 = H.t * H
+    val P_0 = pinv(K_0)
+    val beta = P_0 * H.t * T
 
-    val H = DenseMatrix.zeros[Double](l, L_HIDDEN_NODES)
+    val output = H * beta
+    val output_testing = buildHiddenLayer(a, bias, teX) * beta
 
-    rangeInput.foreach(x => {
-      rangeHiddenLayers.foreach(y => {
-        H(x,y) = hardLim(a(y), bias(y), input(x,1))
-      })
-    })
+    val predictions = denormalizeMatrix(output_testing, maxY, minY)
 
-    val P_o = pinv(H.t * H)
-    val beta = P_o * H.t * T
+    println("====testing-predictions")
+    rangeTestInputs.foreach(i => println(predictions(i, 0) + " " + predictions(i, 1)))
 
-    rangeInput.foreach(x => {
-      val o = rangeHiddenLayers.map(i => bias(i) * hardLim(a(i), bias(i), input(x, 0))).sum
-      val o2 = rangeHiddenLayers.map(i => bias(i) * hardLim(a(i), bias(i), input(x, 1))).sum
-      println(o, o2, ";                 ")
-    })
+    val err_training = sqrt(rangeInputs.map(i => squaredDistance(output(i, ::).t, trY(i, ::).t)).sum)
+    val err_testing = sqrt(rangeTestInputs.map(i => squaredDistance(output_testing(i, ::).t, teY(i, ::).t)).sum)
+    println("====err-training")
+    println(err_training / trY.rows)
+    println("====err-testing")
+    println(err_testing / teY.rows)
   }
 
   def loadCSV(sc: SparkContext, file: String): DenseMatrix[Double] = {
@@ -60,6 +63,63 @@ object OSELM {
     val dim = arr.head.length
     val l = arr.length
 
-    new DenseMatrix(l, dim, arr.flatten)
+    new DenseMatrix(l, dim, arr.transpose.flatten)
+  }
+
+  def denormalizeMatrix(m: DenseMatrix[Double], maxM: DenseVector[Double], minM: DenseVector[Double]): DenseMatrix[Double] = {
+    val rangeCol = 0 until m.cols
+    val rangeRow = 0 until m.rows
+
+    rangeRow.foreach(i => {
+      rangeCol.foreach(j => {
+        m(i, j) = (m(i, j) * (maxM(j) - minM(j))) + minM(j)
+      })
+    })
+
+    m
+  }
+
+  def normalizeMatrix(m: DenseMatrix[Double]): (DenseMatrix[Double], DenseVector[Double], DenseVector[Double]) = {
+    val maxM = max(m(::, *)).t
+    val minM = min(m(::, *)).t
+
+    normalizeMatrix(m, maxM, minM)
+  }
+
+  def normalizeMatrix(m: DenseMatrix[Double], maxM: DenseVector[Double], minM: DenseVector[Double]): (DenseMatrix[Double], DenseVector[Double], DenseVector[Double]) = {
+    val rangeCol = 0 until m.cols
+    val rangeRow = 0 until m.rows
+
+    rangeRow.foreach(i => {
+      rangeCol.foreach(j => {
+        m(i, j) = (m(i, j) - minM(j)) / (maxM(j) - minM(j))
+      })
+    })
+
+    (m, maxM, minM)
+  }
+
+  def G(a: DenseVector[Double], b: Double, x: DenseVector[Double]) = {
+//    tanh(a.t * x + b)
+    sigmoid(a.t * x + b)
+//    signum(a.t * x + b)
+  }
+
+  def buildHiddenLayer(a: DenseMatrix[Double], bias: DenseVector[Double], input: DenseMatrix[Double]): DenseMatrix[Double] = {
+    val l = input.rows
+    val num_hidden_nodes = a.rows
+
+    val rangeInputs = 0 until l
+    val rangeHiddenLayers = 0 until num_hidden_nodes
+
+    val H = DenseMatrix.zeros[Double](l, num_hidden_nodes)
+
+    rangeInputs.foreach(x => {
+      rangeHiddenLayers.foreach(y => {
+        H(x, y) = G(a(y, ::).t, bias(y), input(x, ::).t)
+      })
+    })
+
+    H
   }
 }
